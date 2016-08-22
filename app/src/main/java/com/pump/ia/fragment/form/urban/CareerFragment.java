8b9eb1470869c46d0f6fc1ab81f742da2ede0433
Administrator <634295462@qq.com>
2016-08-23 00:42:42 +0800

    005

---
+++ app/src/main/java/com/pump/ia/fragment/form/urban/CareerFragment.java
@@ -0,0 +1,35 @@
+package com.pump.ia.fragment.form.urban;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.pump.ia.R;
+
+import org.xutils.x;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CareerFragment extends Fragment {
+
+
+    public CareerFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_career, container, false);
+        x.view().inject(this, v);
+
+        return v;
+    }
+
+}
