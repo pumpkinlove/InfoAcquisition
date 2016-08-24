3ea4557eb7f632a606b9837de33183934dcfc54c
Administrator <634295462@qq.com>
2016-08-24 22:51:05 +0800

    009

--- app/src/main/java/com/pump/ia/activity/LoginActivity.java
+++ app/src/main/java/com/pump/ia/activity/LoginActivity.java
@@ -65,29 +65,28 @@ public class LoginActivity extends BaseActivity implements AMapLocationListener 
     @Event(value={R.id.tv_login},type=View.OnClickListener.class)
     private void login(View view) {
 
-//        startActivity(new Intent(LoginActivity.this, MainActivity.class));
-
+        startActivity(new Intent(LoginActivity.this, MainActivity.class));
         //声明mLocationOption对象
-        AMapLocationClientOption mLocationOption = null;
-        AMapLocationClient mlocationClient = new AMapLocationClient(this);
-        //初始化定位参数
-        mLocationOption = new AMapLocationClientOption();
-        //设置返回地址信息，默认为true
-        mLocationOption.setNeedAddress(true);
-        //设置定位监听
-        mlocationClient.setLocationListener(this);
-        //设置定位模式为高精度模式，Battery_Saving为低功耗模式，Device_Sensors是仅设备模式
-        mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
-        //设置定位间隔,单位毫秒,默认为2000ms
-        mLocationOption.setInterval(2000);
-        //设置定位参数
-        mlocationClient.setLocationOption(mLocationOption);
-        // 此方法为每隔固定时间会发起一次定位请求，为了减少电量消耗或网络流量消耗，
-        // 注意设置合适的定位时间的间隔（最小间隔支持为2000ms），并且在合适时间调用stopLocation()方法来取消定位请求
-        // 在定位结束后，在合适的生命周期调用onDestroy()方法
-        // 在单次定位情况下，定位无论成功与否，都无需调用stopLocation()方法移除请求，定位sdk内部会移除
-        // 启动定位
-        mlocationClient.startLocation();
+//        AMapLocationClientOption mLocationOption = null;
+//        AMapLocationClient mlocationClient = new AMapLocationClient(this);
+//        //初始化定位参数
+//        mLocationOption = new AMapLocationClientOption();
+//        //设置返回地址信息，默认为true
+//        mLocationOption.setNeedAddress(true);
+//        //设置定位监听
+//        mlocationClient.setLocationListener(this);
+//        //设置定位模式为高精度模式，Battery_Saving为低功耗模式，Device_Sensors是仅设备模式
+//        mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+//        //设置定位间隔,单位毫秒,默认为2000ms
+//        mLocationOption.setInterval(5000);
+//        //设置定位参数
+//        mlocationClient.setLocationOption(mLocationOption);
+//        // 此方法为每隔固定时间会发起一次定位请求，为了减少电量消耗或网络流量消耗，
+//        // 注意设置合适的定位时间的间隔（最小间隔支持为2000ms），并且在合适时间调用stopLocation()方法来取消定位请求
+//        // 在定位结束后，在合适的生命周期调用onDestroy()方法
+//        // 在单次定位情况下，定位无论成功与否，都无需调用stopLocation()方法移除请求，定位sdk内部会移除
+//        // 启动定位
+//        mlocationClient.startLocation();
 
 
 
